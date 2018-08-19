815d7174bf59a4f0ad3d0c5e6a92e57922a025da
leonardost <leonardost@users.noreply.github.com>
2018-08-19 15:47:32 -0300

    Improve startup shell script

--- 2-board-corner-tracking-java/compile_and_run.sh
+++ 2-board-corner-tracking-java/compile_and_run.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [ ! -d "processing" ]; then
-    mkdir processing
-else
-    rm processing/*
-fi
+create_and_clear_folder() {
+    folder="$1"
+    if [ ! -d "$folder" ]; then
+        mkdir "$folder"
+    else
+        rm "$folder"/*
+    fi
+}
 
-if [ ! -d "output" ]; then
-    mkdir output
-else
-    rm output/*
-fi
+create_and_clear_folder "processing"
+create_and_clear_folder "output"
 
 javac -cp .:opencv-341.jar process_image.java
-java -Djava.library.path=".:/home/leo/" -cp .:opencv-341.jar \
-    process_image datasets/sequence-1
+
+if [ -z "$1" ]; then
+    java -Djava.library.path=".:/home/leo/" -cp .:opencv-341.jar process_image datasets/sequence-1
+else
+    java -Djava.library.path=".:/home/leo/" -cp .:opencv-341.jar process_image "$1"
+fi
+
 rm *.class
