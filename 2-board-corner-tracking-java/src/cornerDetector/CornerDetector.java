eda3380355eb4ef82d14ab0b4aec7a2cb0d64534
leonardost <leonardost@users.noreply.github.com>
2018-09-10 23:17:07 -0300

    Implement circle detection by ellipsis fit in test 2

--- 2-board-corner-tracking-java/src/cornerDetector/CornerDetector.java
+++ 2-board-corner-tracking-java/src/cornerDetector/CornerDetector.java
@@ -4,13 +4,16 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import src.Ponto;
@@ -32,15 +35,12 @@ public class CornerDetector {
         Ponto candidateCornerHarris = detectCornerByHarrisDetection(regionImage);
         // This should have precedence over the Harris Corner detector because a circle
         // in a corner position is great indicative that it is indeed the corner
-        Ponto candidateCornerCircle = detectCornerByCircleDetection(regionImage);
+        // Ponto candidateCornerCircle = detectCornerByCircleDetection(regionImage);
+        Ponto candidateCornerEllipsis = detectCornerByEllipsisFit(regionImage);
 
-        // List<Ponto> candidatePoints = new ArrayList<>();
-        // candidatePoints.add(candidateCornerHarris);
-        // candidatePoints.add(candidateCornerCircle);
-        // Ponto pointClosestToCenterOfRegionOfInterest = getNearestPointToCenterOfRegionOfInterest(candidatePoints);
         Ponto pointClosestToCenterOfRegionOfInterest;
-        if (candidateCornerCircle != null) {
-            pointClosestToCenterOfRegionOfInterest = candidateCornerCircle;
+        if (candidateCornerEllipsis != null) {
+            pointClosestToCenterOfRegionOfInterest = candidateCornerEllipsis;
             System.out.println("Candidate corner found by circle detection in frame " + imageIndex + ": ");
             System.out.println(pointClosestToCenterOfRegionOfInterest);
         } else {
@@ -198,6 +198,88 @@ public class CornerDetector {
         Imgcodecs.imwrite("processing/corner_region_" + cornerIndex + "_circle_detection_step_3_frame" + imageIndex + ".jpg", showCircleDetectionImage);
 
         return getNearestPointToCenterOfRegionOfInterest(possibleCenters);
+    }
+
+    // https://stackoverflow.com/questions/35121045/find-cost-of-ellipse-in-opencv
+    private Ponto detectCornerByEllipsisFit(Mat image)
+    {
+        Mat imageWithEllipsis = image.clone();
+        Mat imageWithBordersDetected = detectBordersIn(image);
+        Mat imageRegions = new Mat();
+        Core.bitwise_not(imageWithBordersDetected, imageRegions);
+        List<MatOfPoint> contours = detectContoursIn(imageRegions);
+
+        Ponto bestCornerCandidate = null;
+        int minimumPointsFound = 999999999;
+        double threshould = 150;
+
+        for (MatOfPoint contour : contours) {
+            if (contour.rows() < 5) continue;
+            MatOfPoint2f contour2f = new MatOfPoint2f();
+            contour.convertTo(contour2f, CvType.CV_32FC2);
+
+            RotatedRect ellipse = Imgproc.fitEllipse(contour2f);
+            if (!isContainedInImage(ellipse)) continue;
+
+            Mat maskContour = new Mat(image.rows(), image.cols(), CvType.CV_8U, new Scalar(0));
+            Mat maskEllipse = new Mat(image.rows(), image.cols(), CvType.CV_8U, new Scalar(0));
+            Mat leftover = new Mat(image.rows(), image.cols(), CvType.CV_8U, new Scalar(0));
+            // The leftover is the difference between the contour found and the ellipse we're trying to fit.
+            // The less leftover there is, the more the ellipse fits the contour.
+            Core.bitwise_xor(maskContour, maskEllipse, leftover);
+
+            int leftoverCount = Core.countNonZero(leftover);
+            Ponto center = new Ponto((int)ellipse.center.x, (int)ellipse.center.y);
+            if (leftoverCount < minimumPointsFound && leftoverCount < threshould) {
+                minimumPointsFound = leftoverCount;
+                bestCornerCandidate = center;
+                Imgproc.ellipse(imageWithEllipsis, ellipse, new Scalar(0, 255, 0), -1);
+            }
+        }
+        Imgcodecs.imwrite("processing/corner_region_" + cornerIndex + "_ellipsis_fit_" + imageIndex + ".jpg", imageWithEllipsis);
+
+        return bestCornerCandidate;
+    }
+
+    private Mat detectBordersIn(Mat image) {
+        Mat imageWithBordersDetected = new Mat();
+        Imgproc.Canny(image, imageWithBordersDetected, 50, 100);
+        Imgproc.dilate(imageWithBordersDetected, imageWithBordersDetected, Mat.ones(3, 3, CvType.CV_32F));
+        return imageWithBordersDetected;
+    }
+
+    private List<MatOfPoint> detectContoursIn(Mat imageWithBordersDetected)
+    {
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(imageWithBordersDetected, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+        removeSmallContours(contours);
+        return contours;
+    }
+
+    private void removeSmallContours(List<MatOfPoint> contours)
+    {
+        for (Iterator<MatOfPoint> it = contours.iterator(); it.hasNext();) {
+            MatOfPoint contour = it.next();
+            if (Imgproc.contourArea(contour) < 450) {
+                it.remove();
+            }
+        }
+    }
+
+    private boolean isContainedInImage(RotatedRect ellipse)
+    {
+        Point[] points = new Point[4];;
+        ellipse.points(points);
+        for (Point point : points) {
+            if (!isPointInsideImage(point)) return false;
+        }
+        return true;
+    }
+
+    private boolean isPointInsideImage(Point point)
+    {
+        return point.x >= 0 && point.x < 100 && point.y >= 0 && point.y < 100;
     }
 
 }
