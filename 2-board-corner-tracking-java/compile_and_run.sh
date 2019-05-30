dc6876fce6b2a7eda73990a0af58224378c808dd
leonardost <leonardost@users.noreply.github.com>
2019-05-29 22:47:18 -0300

    Update compile and run script with new location of opencv-341.jar

--- 2-board-corner-tracking-java/compile_and_run.sh
+++ 2-board-corner-tracking-java/compile_and_run.sh
@@ -12,12 +12,12 @@ create_and_clear_folder() {
 create_and_clear_folder "processing"
 create_and_clear_folder "output"
 
-javac -cp .:opencv-341.jar process_image.java
+javac -cp .:lib/opencv-341.jar process_image.java
 
 if [ -z "$1" ]; then
-    java -Djava.library.path=".:/home/leo/" -cp .:opencv-341.jar process_image datasets/sequence-1 > results.log
+    java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image datasets/sequence-1 > results.log
 else
-    java -Djava.library.path=".:/home/leo/" -cp .:opencv-341.jar process_image "$1" > results.log
+    java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image "$1" > results.log
 fi
 
 find . -name \*.class -type f -delete 
