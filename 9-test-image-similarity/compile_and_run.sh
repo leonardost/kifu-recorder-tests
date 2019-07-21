85be025ddc14807adf86955769af87a0bce89180
leonardost <leonardost@users.noreply.github.com>
2019-07-21 10:26:10 -0300

    Improve experiment 9 to run tests with all test inputs at once

--- 9-test-image-similarity/compile_and_run.sh
+++ 9-test-image-similarity/compile_and_run.sh
@@ -14,6 +14,8 @@ create_and_clear_folder "output"
 
 javac -cp .:lib/opencv-341.jar process_image.java
 
-java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image > results.log
+java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image 1 > results1.log
+java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image 2 > results2.log
+java -Djava.library.path=".:/home/leo/" -cp .:lib/opencv-341.jar process_image 3 > results3.log
 
 find . -name \*.class -type f -delete 
--- 9-test-image-similarity/process_image.java
+++ 9-test-image-similarity/process_image.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,31 +17,34 @@ public class process_image {
 
     static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
-    private final static int NUMBER_OF_IMAGES = 93;
+    private final static int[] NUMbER_OF_IMAGES = { 93, 114, 91 };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<SimilarityCalculatorInterface> similarityCalculators = new ArrayList<>();
         similarityCalculators.add(new TemplateMatching());
         similarityCalculators.add(new FingerprintMatching());
         // similarityCalculators.add(new OrbFeaturesMatching());
 
-        double similarityMatrix[][][] = new double[similarityCalculators.size()][NUMBER_OF_IMAGES][NUMBER_OF_IMAGES];
+        int testNumber = Integer.parseInt(args[0]);
+        String inputFolder = "input" + testNumber;
+        int numberOfImages = NUMbER_OF_IMAGES[testNumber - 1];
 
-        Mat[] images = new Mat[NUMBER_OF_IMAGES];
-        Mat[] originalImages = new Mat[NUMBER_OF_IMAGES];
+        Mat[] images = new Mat[numberOfImages];
+        Mat[] originalImages = new Mat[numberOfImages];
+        double similarityMatrix[][][] = new double[similarityCalculators.size()][numberOfImages][numberOfImages];        
 
-        for (int i = 0; i < NUMBER_OF_IMAGES; i++) {
-            originalImages[i] = readImageFile("input/", i + 1);
-            images[i] = readImageFile("input/", i + 1);
+        for (int i = 0; i < numberOfImages; i++) {
+            originalImages[i] = readImageFile(inputFolder, i + 1);
+            images[i] = readImageFile(inputFolder, i + 1);
             images[i] = convertToGrayscale(images[i], i + 1);
             images[i] = blur(images[i], i + 1);
         }
 
         SimilarityCalculatorInterface similarityCalculator;
-        for (int imageIndex = 0; imageIndex < NUMBER_OF_IMAGES; imageIndex++) {
+        for (int imageIndex = 0; imageIndex < numberOfImages; imageIndex++) {
             System.out.println("Image " + (imageIndex + 1));
 
-            for (int otherImageIndex = imageIndex; otherImageIndex < NUMBER_OF_IMAGES && otherImageIndex < imageIndex + 10; otherImageIndex++) {
+            for (int otherImageIndex = imageIndex; otherImageIndex < numberOfImages && otherImageIndex < imageIndex + 10; otherImageIndex++) {
                 System.out.println("    Image " + (otherImageIndex + 1));
 
                 for (int i = 0; i < similarityCalculators.size(); i++) {
@@ -64,8 +68,26 @@ public class process_image {
         }
     }
 
-    private static Mat readImageFile(String inputFolder, int imageNumber) {
-        return Imgcodecs.imread(inputFolder + "/corner2_frame" + imageNumber + ".png");
+    private static Mat readImageFile(String inputFolder, int imageNumber) throws Exception {
+        String[] filenames = {
+            inputFolder + "/corner2_frame" + imageNumber + ".png",
+            inputFolder + "/ortogonal_2_" + padWithZeroes(imageNumber) + ".png"
+        };
+        for (String filename : filenames) {
+            File file = new File(filename);
+            if (file.exists()) {
+                return Imgcodecs.imread(file.getAbsolutePath());
+            }
+        }
+        throw new Exception("File does not exist");
+    }
+
+    private static String padWithZeroes(int number) {
+        String paddedNumber = Integer.toString(number);
+        while (paddedNumber.length() < 3) {
+            paddedNumber = "0" + paddedNumber;
+        }
+        return paddedNumber;
     }
 
     private static Mat convertToGrayscale(Mat image, int imageIndex)
